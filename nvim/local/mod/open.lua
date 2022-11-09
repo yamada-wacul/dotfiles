c4a0619e917d2c7ebf9799d5eeba7b984d92e9d8
Kyoichiro Yamada <yamada@wacul.co.jp>
2022-11-10 00:22:58 +0900

    fix keymap for gfx

--- nvim/local/mod/open.lua
+++ nvim/local/mod/open.lua
@@ -2,5 +2,5 @@ require("local.mod.open")
 vim.keymap.set("n", "gx", "<plug>(open-cursor-file)", { desc = "Open files or urls under the cursor by special handler" })
 
 vim.keymap.set("n", "gf", 'gF', { remap = false })
-vim.keymap.set("n", "gfv", [[<cmd>execute "vertical normal \<C-W>F"<cr>]], { remap = false })
-vim.keymap.set("n", "gfx", [[<cmd>execute "horizontal normal \<C-W>F"<cr>]], { remap = false })
+vim.keymap.set("n", "gfv", [[<cmd>vertical wincmd F<cr>]], { remap = false })
+vim.keymap.set("n", "gfx", [[<cmd>horizontal wincmd F<cr>]], { remap = false })
