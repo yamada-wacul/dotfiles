db92409c66506b3f94984a69b0070a2d1974ad25
Kyoichiro Yamada <yamada@wacul.co.jp>
2022-11-09 10:09:52 +0900

    split "open" function

--- nvim/local/mod/open.lua
+++ nvim/local/mod/open.lua
@@ -1,3 +1,6 @@
-vim.keymap.set("n", "gx", function()
-    require("local.mod.open").open()
-end, { silent = true, desc = "Open files or urls under the cursor" })
+require("local.mod.open")
+vim.keymap.set("n", "gx", "<plug>(open-cursor-file)", { desc = "Open files or urls under the cursor by special handler" })
+
+vim.keymap.set("n", "gf", 'gF', { remap = false })
+vim.keymap.set("n", "gfv", [[<cmd>execute "vertical normal \<C-W>F"<cr>]], { remap = false })
+vim.keymap.set("n", "gfx", [[<cmd>execute "horizontal normal \<C-W>F"<cr>]], { remap = false })
--- nvim/lua/local/mod/open.lua
+++ nvim/lua/local/mod/open.lua
@@ -1,24 +1,30 @@
 local M = {}
 
-function M.open()
+--- Open the file/url under the cursor with special handler (i.e. xdg-open, wslview, etc...)
+function M.open_cursor()
     local target = vim.fn.expand("<cfile>")
     if target == nil then
         print("No target found at cursor.")
         return
     end
     target = string.gsub(target, [[\.+$]], "")
-    if vim.fn.has("macunix") == 1 then
-        vim.cmd(string.format('silent! !open "%s"', target))
-        vim.cmd("silent! redraw")
+    if vim.loop.os_uname().sysname == "Darwin" == 1 then
+        vim.fn.system('open ' .. vim.fn.shellescape(target))
     elseif vim.fn.executable("wslview") then
-        vim.cmd(string.format('silent! !wslview "%s"', target))
-        vim.cmd("silent! redraw")
+        vim.fn.system('wslview ' .. vim.fn.shellescape(target))
     elseif vim.fn.executable("xdg-open") then
-        vim.cmd(string.format('silent! !xdg-open "%s"', target))
-        vim.cmd("silent! redraw")
+        vim.fn.system('xdg-open ' .. vim.fn.shellescape(target))
     else
-        vim.api.nvim_echo({ { "No executables to view file/url", "ErrorMsg" } }, true, {})
+        vim.api.nvim_echo({ { "No executables to open file/url", "ErrorMsg" } }, true, {})
     end
 end
 
-return M
+vim.api.nvim_create_user_command("OpenCursor", M.open_cursor, {
+    desc = "Open files or urls under the cursor by special handler",
+})
+vim.keymap.set("n", "<plug>(open-cursor-file)", M.open_cursor, {
+    desc = "Open files or urls under the cursor by special handler",
+    silent = true,
+    noremap = true,
+    nowait = true
+})
